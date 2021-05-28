b98a0292aca26473f319ee6fbccd66094e51bf7f
Slashbinslashnoname <slashbinslashnoname@gmail.com>
2021-05-29 03:35:07 +0400

    🐵 Add some ui tweaks and parameters

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ const bearish = require("technicalindicators").bearish;
 require("ansicolor").nice;
 
 //-----------------------------------------------------------------------------
+// Parameters
+
+const TIMEFRAME = "1m";
+const PAIR = "BTC-PERP";
+
+//-----------------------------------------------------------------------------
 async function main() {
 	const index = 4; // [ timestamp, open, high, low, close, volume ]
 
-	const ohlcv = await new ccxt.ftx().fetchOHLCV("BTC-PERP", "1d");
+	const ohlcv = await new ccxt.ftx().fetchOHLCV(PAIR, TIMEFRAME);
 	console.clear();
 	const lastPrice = ohlcv[ohlcv.length - 1][index]; // closing price
 	const series = ohlcv.slice(-200).map((x) => x[index]); // closing price
-	const bitcoinRate = ("₿ = $" + lastPrice).green;
+	const bitcoinRate = (PAIR + " " + lastPrice).green;
+	const timeframe = TIMEFRAME.green;
 	const chart = asciichart.plot(series, {height: 25, padding: "            "});
-	log.yellow("\n" + chart, bitcoinRate, "\n");
+	log.yellow("\n" + chart, "\n\n", timeframe, bitcoinRate, "\n");
 	var fiveLastCandles = {
 		open: [
 			ohlcv[ohlcv.length - 6][1],
@@ -51,14 +58,12 @@ async function main() {
 	};
 
 	log.yellow(
-		"\nBullish candles patterns found ? " +
-			(bullish(fiveLastCandles) ? "Yes" : "No") +
-			"\n"
+		"Bullish candles patterns found on last 5 candles ? " +
+			(bullish(fiveLastCandles) ? "Yes" : "No")
 	);
 	log.yellow(
-		"Bearish candles patterns found  ? " +
-			(bearish(fiveLastCandles) ? "Yes" : "No") +
-			"\n"
+		"Bearish candles patterns found on last 5 candles ? " +
+			(bearish(fiveLastCandles) ? "Yes" : "No")
 	);
 	return true;
 }
